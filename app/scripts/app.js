6ecc1e32896a0f4b8889cca41e66438cee90b1c4
Chris <Christoph07@gmail.com>
2015-09-15 13:44:47 -0400

    First

---
+++ app/scripts/app.js
@@ -0,0 +1,40 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name kjaApp
+ * @description
+ * # kjaApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('kjaApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch',
+    'ui.sortable',
+    'LocalStorageModule'
+  ])
+
+  .config(['localStorageServiceProvider', function(localStorageServiceProvider){
+    localStorageServiceProvider.setPrefix('ls');
+  }])
+
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name kjaApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the kjaApp
+ */
